c280a83abf7ba74939ebb1b489b031ec1f39de71
Brian Tokumoto <bht8183@g.rit.edu>
2025-04-27 22:19:15 -0400

    task 2 finally works

--- layer3_network_code.py
+++ layer3_network_code.py
@@ -1,79 +1,91 @@
-"""
-Mininet L3 topology for HW5
-Routers: rA, rB, rC
-Each router has 2 hosts on its LAN plus one interface on the inter-router /24 mesh
-"""
+# Mininet L3 topology for HW5
+# Routers: rA, rB, rC
+# Each router has 2 hosts on its LAN plus one interface on the inter-router /24 mesh
 
 from mininet.net import Mininet
-from mininet.node import Node
-from mininet.cli  import CLI
+from mininet.node import Node, OVSSwitch
+from mininet.cli import CLI
 from mininet.link import TCLink
-from mininet.log  import setLogLevel
-
+from mininet.log import setLogLevel
 
 class Router(Node):
     "A Node with IP forwarding enabled."
 
     def config(self, **params):
         super().config(**params)
-        # Turn on IP forwarding inside the namespace
         self.cmd('sysctl -w net.ipv4.ip_forward=1')
 
     def terminate(self):
         self.cmd('sysctl -w net.ipv4.ip_forward=0')
         super().terminate()
 
-
 def build_topo():
+    print("*** Creating network")
     net = Mininet(link=TCLink)
 
-    # === Routers ===
-    rA = net.addHost('rA', cls=Router, ip='20.10.100.1/24')
-    rB = net.addHost('rB', cls=Router, ip='20.10.100.2/24')
-    rC = net.addHost('rC', cls=Router, ip='20.10.100.3/24')
+    print("*** Adding routers")
+    rA = net.addHost('rA', cls=Router)
+    rB = net.addHost('rB', cls=Router)
+    rC = net.addHost('rC', cls=Router)
 
-    # === LAN A (20.10.172.128/26) ===
-    hA1 = net.addHost('hA1', ip='20.10.172.129/26', defaultRoute='via 20.10.172.129')
-    hA2 = net.addHost('hA2', ip='20.10.172.130/26', defaultRoute='via 20.10.172.129')
+    print("*** Adding hosts")
+    hA1 = net.addHost('hA1', ip='20.10.172.130/26', defaultRoute='via 20.10.172.129')
+    hA2 = net.addHost('hA2', ip='20.10.172.131/26', defaultRoute='via 20.10.172.129')
+    hB1 = net.addHost('hB1', ip='20.10.172.2/25', defaultRoute='via 20.10.172.1')
+    hB2 = net.addHost('hB2', ip='20.10.172.3/25', defaultRoute='via 20.10.172.1')
+    hC1 = net.addHost('hC1', ip='20.10.172.194/27', defaultRoute='via 20.10.172.193')
+    hC2 = net.addHost('hC2', ip='20.10.172.195/27', defaultRoute='via 20.10.172.193')
 
-    # === LAN B (20.10.172.0/25) ===
-    hB1 = net.addHost('hB1', ip='20.10.172.1/25',  defaultRoute='via 20.10.172.1')
-    hB2 = net.addHost('hB2', ip='20.10.172.2/25',  defaultRoute='via 20.10.172.1')
+    print("*** Adding switches")
+    s1 = net.addSwitch('s1', cls=OVSSwitch, failMode='standalone')
+    s2 = net.addSwitch('s2', cls=OVSSwitch, failMode='standalone')
+    s3 = net.addSwitch('s3', cls=OVSSwitch, failMode='standalone')
+    s4 = net.addSwitch('s4', cls=OVSSwitch, failMode='standalone')
 
-    # === LAN C (20.10.172.192/27) ===
-    hC1 = net.addHost('hC1', ip='20.10.172.193/27', defaultRoute='via 20.10.172.193')
-    hC2 = net.addHost('hC2', ip='20.10.172.194/27', defaultRoute='via 20.10.172.193')
+    print("*** Creating links")
+    net.addLink(s1, rA, intfName2='rA-eth1')
+    net.addLink(s2, rB, intfName2='rB-eth1')
+    net.addLink(s3, rC, intfName2='rC-eth1')
 
-    # === Switches (one per LAN) ===
-    sA = net.addSwitch('sA')
-    sB = net.addSwitch('sB')
-    sC = net.addSwitch('sC')
-    sBackbone = net.addSwitch('sBB')          # connects the 3 routers together
+    net.addLink(s1, hA1)
+    net.addLink(s1, hA2)
+    net.addLink(s2, hB1)
+    net.addLink(s2, hB2)
+    net.addLink(s3, hC1)
+    net.addLink(s3, hC2)
 
-    # --- Wire the LANs ---
-    net.addLink(sA, rA, intfName2='rA-eth1', params2={'ip': '20.10.172.129/26'})
-    net.addLink(sB, rB, intfName2='rB-eth1', params2={'ip': '20.10.172.1/25'})
-    net.addLink(sC, rC, intfName2='rC-eth1', params2={'ip': '20.10.172.193/27'})
+    net.addLink(s4, rA, intfName2='rA-eth2')
+    net.addLink(s4, rB, intfName2='rB-eth2')
+    net.addLink(s4, rC, intfName2='rC-eth2')
 
-    net.addLink(sA, hA1); net.addLink(sA, hA2)
-    net.addLink(sB, hB1); net.addLink(sB, hB2)
-    net.addLink(sC, hC1); net.addLink(sC, hC2)
-
-    # --- Inter-router mesh (/24) ---
-    net.addLink(sBackbone, rA, intfName2='rA-eth2')   # already has 20.10.100.1/24
-    net.addLink(sBackbone, rB, intfName2='rB-eth2')   # 20.10.100.2/24
-    net.addLink(sBackbone, rC, intfName2='rC-eth2')   # 20.10.100.3/24
-
+    print("*** Building network")
     net.build()
 
-    # Quick reachability test inside each LAN only
-    print("\n=== pingall (intra-LAN will reply, inter-LAN will fail) ===")
-    net.pingAll()
+    # get your routers
+    rA, rB, rC = net.get('rA','rB','rC')
 
-    CLI(net)   # drop to CLI so you can run pingall again, ifconfig, etc.
+    # 1) Assign LAN‐side IPs (your subnets from Task 1)
+    rA.setIP('20.10.172.129/26', intf='rA-eth1')
+    rB.setIP('20.10.172.1/25',  intf='rB-eth1')
+    rC.setIP('20.10.172.193/27',intf='rC-eth1')
+
+    # 2) Assign backbone IPs as you already had
+    rA.setIP('20.10.100.1/24', intf='rA-eth2')
+    rB.setIP('20.10.100.2/24', intf='rB-eth2')
+    rC.setIP('20.10.100.3/24', intf='rC-eth2')
+
+    # 3) Now actually start the network (so switches come up)
+    net.start()
+
+    # 4) Test intra-LAN reachability
+    print("\n=== Testing intra-LAN connectivity only ===")
+    net.ping([ net.get('hA1'), net.get('hA2') ])
+    net.ping([ net.get('hB1'), net.get('hB2') ])
+    net.ping([ net.get('hC1'), net.get('hC2') ])
+
+    CLI(net)
     net.stop()
-
-
+    
 if __name__ == '__main__':
     setLogLevel('info')
     build_topo()
